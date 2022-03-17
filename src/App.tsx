fd2bd9e9c894736060d6dabde0be4ad0637cdd94
MARCOS ANATHAN GUEDES VASCONCELOS <111960@pmenos.com.br>
2022-03-17 19:04:56 -0300

    feat: clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef} from 'react';
 import './App.css';
 import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
